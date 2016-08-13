b4debdc8f420594143ef4ddd07493b627955f277
MikaelFeher <mikael.feher@gmail.com>
2016-08-13 16:35:02 +0200

    scaffolding ionic app WIP intergrating cooper logic

---
+++ www/js/app.js
@@ -0,0 +1,47 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  .state('app.about', {
+    url: '/about',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/about/about.html'
+      }
+    }
+  })
+  .state('app.test', {
+   url: '/test',
+   views: {
+     'menuContent': {
+       templateUrl: 'templates/test/test.html',
+       controller: 'TestController'
+     }
+   }
+ });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/about');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  $scope.loginData = {};
+
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+.controller('TestController', function($scope) {
+  $scope.data = {};
+  $scope.cooper_calc = function() {
+    var person = new Person({
+      gender: $scope.data.gender,
+      age: $scope.data.age
+    });
+    person.cooper_result($scope.data.distance);
+    $scope.person = person;
+    console.log($scope.person);
+  };
+});
